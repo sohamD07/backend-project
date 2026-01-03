54ef397b2c94e5a4874b97c00c6b076d5409195e
Soham <soham.dhawale@elastic.run>
2026-01-03 18:52:24 +0530

    routes and controller setup

--- src/app.js
+++ src/app.js
@@ -18,4 +18,13 @@ app.use(express.static("public"));
 
 app.use(cookieParser());
 
+// routes
+import userRouter from "./routes/user.routes.js";
+
+
+// routes declaration
+app.use("/api/v1/users", userRouter);
+
+// http://localhost:8080/api/v1/users/register
+
 export default app;
