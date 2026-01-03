060776223f7456224e5fe8506fd2e7ee60593859
Soham <soham.dhawale@elastic.run>
2026-01-03 14:25:07 +0530

    util code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import dotenv from "dotenv";
+import app from "./app.js";
 import connectDB from "./db/index.js";
 
 dotenv.config({path: './env'});
 
 
 connectDB()
+    .then(() => {
+        const PORT = process.env.PORT || 8000;
+        app.listen(PORT, () => {
+            console.log(`Server is running at port : ${PORT}`);
+        })
+    })
+    .catch((err) => {
+        console.log("MONGODB connection failed !!! ", err);
+    })
 
 
 
